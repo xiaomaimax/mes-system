e51fc8009b6f92b2f16015cd79f898a283fb657c
Max MES <liumin@mes-system.local>
2026-04-05 00:24:07 +0800

    feat: 添加系统文档功能 + 修复 API 路径问题

---
+++ client/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function(app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://127.0.0.1:5001",
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      logLevel: "debug"
+    })
+  );
+};
