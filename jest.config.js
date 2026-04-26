d4afb2ee1c18c87852806091013b8a17eb65d38b
Max <10044750@qq.com>
2026-04-26 11:46:12 +0800

    chore: add jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.js'],
+  // 只测试后端 server/ 目录，前端 React 测试需要额外配置
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/client/',       // 前端 React 测试需要 Babel/JSDOM，另行处理
+  ],
   collectCoverageFrom: [
     'server/**/*.js',
     '!server/app.js',
